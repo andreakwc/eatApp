27fbd45b75a9f56d13f9ecac10c8b040c06f5d4f
Andrea C <chan.andreakw@gmail.com>
2015-02-07 08:26:25 -0500

    it kind of works lol

--- app.py
+++ app.py
@@ -1,19 +1,7 @@
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, render_template, url_for, request
 import requests
-import gdata.spreadsheet.service
-import gdata.spreadsheet
-import gdata.service
-import gdata.spreadsheet.text_db
-import atom.service
-import getopt
 import sys
-import re, os, os.path
-import string
-import socket
 import json
-import oauth2
-import urllib
-import urllib2
 import pprint
 
 app = Flask(__name__)
@@ -33,57 +21,61 @@ SEARCH_LIMIT = 3
 
 @app.route("/hello")
 def hello():
- 	return "Hey There! Hello World!"
+    return "Hey There! Hello World!"
 
 # def __init__(self, email, password):
-# 	self.gd_client = gdata.spreadsheet.service.SpreadsheetsService()
-# 	self.gd_client.email = eatApp2015@gmail.com
-# 	self.gd_client.password = dining2015
-# 	self.gd_client.source = "eatApp"
-# 	self.gd_client.ProgrammaticLogin()
-	#self.curr_key = ""
-	#self.curr_wksht_id = ""
-	#self.list_feed = None
+#   self.gd_client = gdata.spreadsheet.service.SpreadsheetsService()
+#   self.gd_client.email = eatApp2015@gmail.com
+#   self.gd_client.password = dining2015
+#   self.gd_client.source = "eatApp"
+#   self.gd_client.ProgrammaticLogin()
+    #self.curr_key = ""
+    #self.curr_wksht_id = ""
+    #self.list_feed = None
 
 #@app.route("/search/<search_query>")
-#def search(search_query):	
-#	return search_query
+#def search(search_query):  
+#   return search_query
 
 # @app.route("/select", methods=["GET", "POST"])
 # def search():
-# 	if request.method == "POST":
-# 		url = "https://docs.google.com/spreadsheet/ccc?key=0At7RkuwrpNM0dDNfNGd5STk2TXQ1aHZ2elNqUFVIM0E&usp=drive_web&pli=1#gid=0" + request.form["user_search"]
-# 		response_dict = requests.get(url).json()
-# 		return jsonify(response_dict)
-# 	else:
-# 		return render_template("search.html")
+#   if request.method == "POST":
+#       url = "https://docs.google.com/spreadsheet/ccc?key=0At7RkuwrpNM0dDNfNGd5STk2TXQ1aHZ2elNqUFVIM0E&usp=drive_web&pli=1#gid=0" + request.form["user_search"]
+#       response_dict = requests.get(url).json()
+#       return jsonify(response_dict)
+#   else:
+#       return render_template("search.html")
 
 
 @app.route("/select", methods = ["GET", "POST"])
 def select():
-	if request.method == "POST":
-		where = request.form["Where"]
-		what = request.form["What"]
-		places = []
+    if request.method == "POST":
+        where = request.form["Where"]
+        what = request.form["What"]
+        places = []
 
-		with open(url_for('static', filename='js/restaurants.json')) as json_file:
-			restaurants = json.reads(json_file.read())["restaurants"]
+        #with open(url_for('static', filename='js/restaurants.json')) as json_file:
+            #restaurants = json.reads(json_file.read())["restaurants"]
 
-		for r in restaurants:
-			if r['area'] == where and r['cuisine'] == what:
-				places.append(r['name'])
-		return render_template("results.html", elements = places)
-	else:
-		return render_template("search.html")
+        with open('static/js/restaurants.json') as json_file:
+            restaurants = json.loads(json_file.read())["restaurants"]
+            for r in restaurants:
+                if r['Area'] == where and r['Cuisine'] == what:
+                    places.append(r['Name'])
+        return render_template("results.html", elements = places)
+    return render_template("search.html")
 
+@app.route("/results")
+def results():
+    render_template("results.html")
 
 @app.errorhandler(404)
 def page_not_found(error):
-	return "Sorry, this page was not found", 404
+    return "Sorry, this page was not found", 404
 
 @app.route("/search/exception")
 def exception():
-	return "Gotcha!"
+    return "Gotcha!"
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0")
